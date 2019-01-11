23a979d6c7efacd0b0b9a7ba644f5787ccaf652c
Anillina <lina1geis@gmail.com>
2019-01-11 09:09:49 +0100

    Hallo!

--- src/model/Quest.java
+++ src/model/Quest.java
@@ -5,13 +5,50 @@ import akkgframework.model.fundamental.GraphicalObject;
 
 public class Quest{
 
-    private List<String> task;
+    private int number;
+    private boolean done;
 
-    public Quest(){
-        task=new List<>();
+    private String task;
+    private Player player;
+
+    public Quest(Player player,String task, int number, boolean done){
+        this.player=player;
+        fillQuests(task, number, done);
     }
 
-    public void fillQuests(String task){
-        this.task.append(task);
+    public void fillQuests(String task, int number, boolean done){
+        this.task=task;
+        this.number=number;
+        this.done=done;
+    }
+
+    public void check(){
+        /*if(player.getTask(task)>=number){
+            setDone(true);
+        }*/
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
     }
 }
