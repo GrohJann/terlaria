56175870ee0369d52a0ce91cf76892e1b0d4dd4f
Anillina <lina1geis@gmail.com>
2019-01-27 19:59:44 +0100

    quest beedndet und kommentare

--- src/control/ProgramController.java
+++ src/control/ProgramController.java
@@ -12,6 +12,8 @@ import model.textures.Background;
 import model.textures.blocks.Grass;
 import model.textures.entitys.Player;
 
+import java.awt.*;
+
 /**
  * Ein Objekt der Klasse ProgramController dient dazu das Programm zu steuern. Die updateProgram - Methode wird
  * mit jeder Frame im laufenden Programm aufgerufen.
@@ -31,6 +33,7 @@ public class ProgramController {
     private Terrain terrain;
     private Player player;
     private Inventory inventory;
+    GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
 
     private Grass grass;
 
@@ -81,11 +84,13 @@ public class ProgramController {
     public void updateProgram(double dt){
         programTimer += dt;
         // ******************************************* Ab hier euer eigener Code! *******************************************
-        
+
         terrain.handleCollision(player,dt);
 
         soundCon.update(dt);
 
+        //überprüfe der quests
+        //folgen wenn eine quest erfüllt ist
         if(!quests.isEmpty()) {
             quests.front().check();
             if (quests.front().isDone()) {
@@ -104,35 +109,38 @@ public class ProgramController {
         }
     }
 
+    /**
+     *Qustes werden erstellt
+     */
     private void createQuests(){
         quests = new Queue<>();
-        Quest newQuest = new Quest(player,"Go to the right", "x",1, 1590,"right",false,true);
+        Quest newQuest = new Quest(player,"Go to the right - Press D", "x",1, gd.getDisplayMode().getWidth()-100,"right",false,true);
         quests.enqueue(newQuest);
-        Quest newQuest1 = new Quest(player,"Spend time in the game", "time",10, 10 ,"",false,false);
+        Quest newQuest1 = new Quest(player,"Spend time in the game - Just wait", "time",10, 10 ,"",false,false);
         quests.enqueue(newQuest1);
-        Quest newQuest2 = new Quest(player,"Go to the left", "x",1, 150,"left",false,true);
+        Quest newQuest2 = new Quest(player,"Go to the left - Press A", "x",1,5,"left",false,true);
         quests.enqueue(newQuest2);
-        Quest newQuest3 = new Quest(player,"Go to the right", "x",1, 1850,"right",false,false);
+        Quest newQuest3 = new Quest(player,"Go to the right", "x",1, gd.getDisplayMode().getWidth()-100,"right",false,false);
         quests.enqueue(newQuest3);
-        Quest newQuest4 = new Quest(player,"Go to the left", "x",1,200, "left",false,false);
+        Quest newQuest4 = new Quest(player,"Go to the left", "x",1,5, "left",false,false);
         quests.enqueue(newQuest4);
         Quest newQuest5 = new Quest(player,"Spend time in the game", "time",60,60, "",false,false);
         quests.enqueue(newQuest5);
-        Quest newQuest6 = new Quest(player,"Go to the right", "x",1, 1200,"right",false,false);
+        Quest newQuest6 = new Quest(player,"Go to the right", "x",1, gd.getDisplayMode().getWidth()-100,"right",false,false);
         quests.enqueue(newQuest6);
-        Quest newQuest7 = new Quest(player,"Go to the left", "x",1, 150,"left",false,false);
+        Quest newQuest7 = new Quest(player,"Go to the left", "x",1, 5,"left",false,false);
         quests.enqueue(newQuest7);
-        Quest newQuest8 = new Quest(player,"Go to the right", "x",1, 1500,"right",false,false);
+        Quest newQuest8 = new Quest(player,"Go to the right", "x",1, gd.getDisplayMode().getWidth()-100,"right",false,false);
         quests.enqueue(newQuest8);
         Quest newQuest9 = new Quest(player,"Spend time in the game", "time",120, 120,"",false,false);
         quests.enqueue(newQuest9);
-        Quest newQuest10 = new Quest(player,"Go to the left", "x",1, 250,"left",false,false);
+        Quest newQuest10 = new Quest(player,"Go to the left", "x",1, 5,"left",false,false);
         quests.enqueue(newQuest10);
-        Quest newQuest11 = new Quest(player,"Go to the right", "x",1,1560,"right",false,false);
+        Quest newQuest11 = new Quest(player,"Go to the right", "x",1,gd.getDisplayMode().getWidth()-100,"right",false,false);
         quests.enqueue(newQuest11);
         Quest newQuest12 = new Quest(player,"Spend time in the game", "time",300,300, "",false,false);
         quests.enqueue(newQuest12);
-        Quest newQuest13 = new Quest(player,"Go to the left", "x",1, 150,"left",false,false);
+        Quest newQuest13 = new Quest(player,"Go to the left", "x",1, 5,"left",false,false);
         quests.enqueue(newQuest13);
     }
 
